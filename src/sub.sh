f7061a556babd45968ad3576e1c10a93ec05cca0
TO/GA <chenyi2000@zju.edu.cn>
2021-11-08 16:52:59 +0800

    teach slurm how to utilize CPU fully

--- src/sub.sh
+++ src/sub.sh
@@ -5,5 +5,6 @@
 #SBATCH --cpus-per-task=32
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-mpirun ./main 0.005  ../data/ipcc_gauge_24_72  24 24 24 72  12 24 24 36
-# mpirun --bind-to none ./main 0.005  ../data/ipcc_gauge_24_72  24 24 24 72  12 24 24 36
+export OMP_PROC_BIND=true
+# mpirun ./main 0.005  ../data/ipcc_gauge_24_72  24 24 24 72  12 24 24 36
+mpirun --report-bindings -rf rankfile --bind-to none ./main 0.005  ../data/ipcc_gauge_24_72  24 24 24 72  12 24 24 36
