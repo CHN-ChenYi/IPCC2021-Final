76940f5dc7ae9fc61a9bda351abaf93dd9b3ebb4
yzhblind <yangzihan2000@outlook.com>
2021-11-09 20:54:35 +0800

    simd in mpi_operator

--- include/operator_mpi.h
+++ include/operator_mpi.h
@@ -8,6 +8,9 @@
 
 #include <mpi.h>
 #include <complex>
+#include "fast_complex.hpp"
+
+const __m256d odd_vec = {1.0, -1.0, 1.0, -1.0};
 
 template <typename T>
 double norm_2(const T &s)
@@ -15,11 +18,21 @@ double norm_2(const T &s)
     //    int rank;
     //    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     //if (rank==0){
-    std::complex<double> s1(0.0, 0.0);
-    for (int i = 0; i < s.size; i++) {
-        s1 += s.A[i] * conj(s.A[i]);
+    // std::complex<double> s1(0.0, 0.0);
+    __m256d s1 = _mm256_setzero_pd();
+    // for (int i = 0; i < s.size; i++) {
+    //     s1 += s.A[i] * conj(s.A[i]);
+    // }
+    for (int i = 0; i < s.size; i += 2) {
+        __m256d vec1 = _mm256_load_pd((double *) &s.A[i]);
+        __m256d vec2 = _mm256_mul_pd(vec1, odd_vec);
+        s1 += complex_256_mul(vec1, vec2);
     }
-    double sum_n = s1.real();
+    __m128d res1 = _mm256_extractf128_pd(s1, 0);
+    __m128d res2 = _mm256_extractf128_pd(s1, 1);
+    res1 = _mm_add_pd(res1, res2);
+    double sum_n = _mm_cvtsd_f64(res1);
+    // double sum_n = s1.real();
     double sum;
     MPI_Reduce(&sum_n, &sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
     MPI_Bcast(&sum, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
