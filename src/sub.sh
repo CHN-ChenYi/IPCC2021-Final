765493067cd94cc960aaebfabd1379b62f231311
TO/GA <chenyi2000@zju.edu.cn>
2021-11-09 10:48:49 +0800

    change proc num from 4 to 16

--- src/sub.sh
+++ src/sub.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #SBATCH -p amd_256
 #SBATCH --nodes=2
-#SBATCH --ntasks-per-node=2
-#SBATCH --cpus-per-task=32
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=8
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export OMP_PROC_BIND=true
 # mpirun --report-bindings -rf rankfile --bind-to none ./main 0.005  ../data/ipcc_gauge_24_72  24 24 24 72  12 24 24 36
-mpirun -rf rankfile --bind-to none ./main 0.005  ../data/ipcc_gauge_24_72  24 24 24 72  12 24 24 36
+# mpirun -rf rankfile --bind-to none ./main 0.005  ../data/ipcc_gauge_24_72  24 24 24 72  12 24 24 36
+mpirun -rf rankfile --bind-to none ./main 0.005  ../data/ipcc_gauge_24_72  24 24 24 72  24 24 12 9
