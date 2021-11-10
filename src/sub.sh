de3e4388791174d92af9eadf8f262ee7bc1a197c
TO/GA <chenyi2000@zju.edu.cn>
2021-11-10 11:26:45 +0800

    comment case1/2 in sub.sh

--- src/sub.sh
+++ src/sub.sh
@@ -2,6 +2,6 @@
 #SBATCH -p amd_256
 #SBATCH -N 2
 #SBATCH -n 128
-mpirun ./main 0.005  ../data/ipcc_gauge_24_72  24 24 24 72  24 12 3 9
-mpirun ./main 0.005  ../data/ipcc_gauge_32_64  32 32 32 64  32 32 16 1
+# mpirun ./main 0.005  ../data/ipcc_gauge_24_72  24 24 24 72  24 12 3 9
+# mpirun ./main 0.005  ../data/ipcc_gauge_32_64  32 32 32 64  32 32 16 1
 mpirun ./main 0.005  ../data/ipcc_gauge_48_96  48 48 48 96  48 24 3 24
