48a523f4c97b8246c321803d0c93460f7483f519
TO/GA <chenyi2000@zju.edu.cn>
2021-11-10 11:23:49 +0800

    optimize subgrid

--- src/sub.sh
+++ src/sub.sh
@@ -2,6 +2,6 @@
 #SBATCH -p amd_256
 #SBATCH -N 2
 #SBATCH -n 128
-# mpirun ./main 0.005  ../data/ipcc_gauge_24_72  24 24 24 72  6 12 12 9
-# mpirun ./main 0.005  ../data/ipcc_gauge_32_64  32 32 32  64  8 16 16 8
-mpirun ./main 0.005  ../data/ipcc_gauge_48_96  48 48 48 96  12 24 24 12
+mpirun ./main 0.005  ../data/ipcc_gauge_24_72  24 24 24 72  24 12 3 9
+mpirun ./main 0.005  ../data/ipcc_gauge_32_64  32 32 32 64  32 32 16 1
+mpirun ./main 0.005  ../data/ipcc_gauge_48_96  48 48 48 96  48 24 3 24
